cc34cfcc502efea635ca712093badd0ea3f314f8
Martin Jul <martin@mjul.com>
2011-06-04 23:19:25 +0200

    Repo created.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject ndc2011 "0.0.1-SNAPSHOT"
+  :description "Top Ten Things to Learn from Clojure that will make you a better developer in any language." 
+  :dependencies [[clojure "1.2.0"]
+		 [clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.3.1"]
+		     [marginalia "0.5.1"]]
+  :tasks [marginalia.tasks]
+  )
